09ee7e7aa1b1c134717861e799a79c5bbcfb8a37
Yesha Patel <yesha@email.unc.edu>
2021-09-04 16:21:44 -0400

    repeat beat completed

--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -1,6 +1,19 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730402890"
 
 
-# Begin your solution here...
+beat: str = input("What beat do you want to repeat? ")
+times: int = int(input("How many times do you want to repeat it? "))
+beat_string: str = beat
+counter: int = 1
+
+
+while counter < times:
+    beat_string = beat_string + " " + beat
+    counter = counter + 1
+
+if times <= 0:
+    print("No beat...")
+else:
+    print(beat_string)
